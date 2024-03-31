d9d5a89a418611e44a30181d64fd80d3a5cf6f9f
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-03-30 21:42:18 -0500

    feat: Add initialization functions
    These functions initialize the entire game.
    
    The ship initialization will only place ships in valid configurations.

--- src/battleship.c
+++ src/battleship.c
@@ -19,5 +19,46 @@ typedef struct game {
 
 int lengths[5] = {2, 3, 3, 4, 5};
 
+void init_board(board *game_board) {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      game_board->player_1_guesses[i][j] = false;
+      game_board->player_2_guesses[i][j] = false;
+    }
+  }
+
+  game_board->player_1_last_guess[0] = -1;
+  game_board->player_1_last_guess[1] = -1;
+  game_board->player_2_last_guess[0] = -1;
+  game_board->player_2_last_guess[1] = -1;
+}
+
+void init_ships(board *game_board) {
+  for (int i = 0; i < 5; i++) {
+    game_board->player_1_ships[i].initialized = false;
+    game_board->player_2_ships[i].initialized = false;
+  }
+
+  for (int i = 0; i < 5; i++) {
+    ship new_ship;
+    new_ship.length = lengths[i];
+    random_position(&new_ship);
+    while (check_ships_collision(game_board->player_1_ships, new_ship)) {
+      random_position(&new_ship);
+    }
+    game_board->player_1_ships[i] = new_ship;
+  }
+
+  for (int i = 0; i < 5; i++) {
+    ship new_ship;
+    new_ship.length = lengths[i];
+    random_position(&new_ship);
+    while (check_ships_collision(game_board->player_2_ships, new_ship)) {
+      random_position(&new_ship);
+    }
+    game_board->player_2_ships[i] = new_ship;
+  }
+}
+
 int main_game(game *game) {
 }
